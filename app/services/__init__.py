8633585049397565d7bc65084479fe250d2e94ec
ac (sourcetree) <alchang@Pobox.com>
2025-07-15 23:51:44 -0500

    fix import error: remove ModelCatalogService from services init

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -3,6 +3,5 @@ Services module for AIdeator application.
 """
 
 from .kubernetes_service import KubernetesService
-from .model_catalog import ModelCatalogService
 
-__all__ = ["KubernetesService", "ModelCatalogService"]
+__all__ = ["KubernetesService"]
